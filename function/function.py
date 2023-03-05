825cd1bfd136152d9d94ea1cc0b262bac6461a1d
Dat <dat.lemindast@gmail.com>
2023-03-05 17:26:23 +0700

    change

--- function/function.py
+++ function/function.py
@@ -14,20 +14,22 @@ class fuzzy_score:
     def push_meta(self, meta_data):
         self.list.append(meta_data)
     
-    def insertionSort(self):
-        if (n := len(self.list)) <= 1: return
-        for i in range(1, n):
-            key = self.list[i]
-            j = i-1
-            while j >=0 and key.score < self.list[j].score :
-                self.list[j+1] = self.list[j]
-                j -= 1
-            self.list[j+1] = key
+    def insertion_sort(self):
+        for item in self.list:
+            if (n := len(item.meta_data_lake_score)) <= 1: 
+                return
+            for index in range(1, n):
+                key = item.meta_data_lake_score[index]
+                jindex = index-1
+                while jindex >=0 and key.score < item.meta_data_lake_score[jindex].score :
+                    item.meta_data_lake_score[jindex+1] = item.meta_data_lake_score[jindex]
+                    jindex -= 1
+                item.meta_data_lake_score[jindex+1] = key
 
 def fuzzy_attributes(meta_a, meta_b, alpha, beta):
     weight_a = fuzz(meta_a.tag_name, meta_b.tag_name)
     weight_b = fuzz(meta_a.value,meta_b.actual_value)
-    meta_a.score.append(item_score(meta_b.tag_name, weight_a * alpha + weight_b * beta)/(alpha + beta))
+    return item_score((meta_b.tag_name, weight_a * alpha + weight_b * beta)/(alpha + beta))
 
 def processing():
     array_metadata_score = fuzzy_score()
@@ -35,8 +37,13 @@ def processing():
     meta_xml_1 = parse_metadata_xml(xml_global)
     meta_xml_2 = parse_metadata_xml(xml_recipe)
     for item in meta_controlplan:
-        fuzzy_attributes
-    
+        for item_item in meta_xml_1:
+            item.meta_data_lake_score.append(fuzzy_attributes(item, item_item, 0.7, 0.3))
+        for item_item_2 in meta_xml_2:
+            item.meta_data_lake_score.append(fuzzy_attributes(item, item_item_2, 0.7, 0.3))
+        array_metadata_score.push_meta(item)
+    array_metadata_score.insertion_sort()
+    return array_metadata_score
     
 
 
