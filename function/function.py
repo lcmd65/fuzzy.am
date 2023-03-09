a349a82549a89df0a9313a3899e46d0d5d6391fc
Dat <dat.lemindast@gmail.com>
2023-03-09 19:36:11 +0700

    change

--- function/function.py
+++ function/function.py
@@ -8,13 +8,13 @@ class item_score:
         # use dictionary here
 
 class fuzzy_score:
-    def __init__(self):
+    def __init__(self): # init arr
         self.list = []
     
-    def push_meta(self, meta_data):
+    def push_meta(self, meta_data): # push item
         self.list.append(meta_data)
     
-    def insertion_sort(self):
+    def insertion_sort(self): # sort the score array after fitting with fuzzy matching
         for item in self.list:
             if (n := len(item.meta_data_lake_score)) <= 1: 
                 return
@@ -26,12 +26,12 @@ class fuzzy_score:
                     jindex -= 1
                 item.meta_data_lake_score[jindex+1] = key
 
-def fuzzy_attributes(meta_a, meta_b, alpha, beta):
+def fuzzy_attributes(meta_a, meta_b, alpha, beta): # unit caculate of fuzzy matching 
     weight_a = fuzz(meta_a.tag_name, meta_b.tag_name)
     weight_b = fuzz(meta_a.value,meta_b.actual_value)
     return item_score((meta_b.tag_name, weight_a * alpha + weight_b * beta)/(alpha + beta))
 
-def processing():
+def processing(): # parsing and caculate score of fuzzy matching
     array_metadata_score = fuzzy_score()
     meta_controlplan = metadata_db_controlplan(370)
     meta_xml_1 = parse_metadata_xml(xml_global)
@@ -44,7 +44,7 @@ def processing():
         array_metadata_score.push_meta(item)
     array_metadata_score.insertion_sort()
     return array_metadata_score
-    
+
 
 
 
